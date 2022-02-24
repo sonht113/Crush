e66072b1e9ecfb36ef1c2bde1732cb4b5a6001d9
sonht113 <trongson18101999@gmail.com>
2022-02-24 19:46:27 +0700

    [hotrongson] update demototinh

--- js/myJs.js
+++ js/myJs.js
@@ -1,13 +1,13 @@
 const textConfig = {
-  text1: "He luu Chị gái xinh đẹp!",
-  text2: "Lần đầu nhìn thấy chị em đã muốn nói chuyện nhưng không biết phải làm thế nào để bắt đầu nên phải dùng cách này. Hi vọng chị sẽ thích hihi!!!",
-  text3: "Chị cho em làm quen nhé? ._.",
-  text4: "Nếu chị thoát mà không trả lời thì làm người yêu em đó nha hehe",
-  text5: "Cậu mơ à???",
-  text6: "Chị đồng ý <3",
-  text7: "Cứ nghĩ là thiên thần không có thật. Nhưng điều đó đã sai sau khi gặp được chị ><",
-  text77: "Đưa bàn tay này em nắm cho. Em sẽ đưa chị đến với hạnh phúc hihi",
-  text78: "Bầu trời đẹp thật nhưng chỉ đẹp khi có thêm chị ở đó thôi <3",
+  text1: "sdafdsfdsfdsafdsfdsfdsfdsf",
+  text2: "dsfdsfdsfdsafdsfdsfsdfd",
+  text3: "sdfdsfdsfdsf",
+  text4: "sdfdsfdsf",
+  text5: "fdsfdsf?",
+  text6: "Cdfdfddf",
+  text7: "dsfdsfdsfdsfds",
+  text77: "fdsfdsfdsfdsf",
+  text78: "dfsdafdsfsdfdsfdsfds",
   text8: "Tiếp tục nào hihi -> ",
   text9: "Vì cậu đẹp try vlllll",
   text10: "Tớ biết mà ^^ Yêu cậu 300.000",
@@ -40,7 +40,7 @@ $(document).ready(function () {
       imageUrl: "img/cuteCat.jpg",
       imageWidth: 300,
       imageHeight: 300,
-      background: '#fff url("img/iput-bg.jpg")',
+      background: '#fff url("")',
       imageAlt: "Custom image",
     }).then(function () {
       $(".content").show(200);
@@ -102,7 +102,7 @@ $(document).ready(function () {
                     left top
                     no-repeat
                   `,
-      imageUrl:"./img2/33.jpg",
+      imageUrl:"",
       confirmButtonColor: "#3085d6",
       cancelButtonColor: "#d33",
       confirmButtonColor: "#fe8a71",
@@ -121,7 +121,7 @@ $(document).ready(function () {
                         left top
                         no-repeat
                       `,
-          imageUrl:"./img2/43.jpg",
+          imageUrl:"",
           confirmButtonColor: "#3085d6",
           cancelButtonColor: "#d33",
           confirmButtonColor: "#fe8a71",
@@ -140,14 +140,14 @@ $(document).ready(function () {
                             left top
                             no-repeat
                           `,
-              imageUrl:"./img2/50.jpg",
+              imageUrl:"",
               confirmButtonColor: "#3085d6",
               cancelButtonColor: "#d33",
               confirmButtonColor: "#fe8a71",
               cancelButtonColor: "#f6cd61",
               confirmButtonText: textConfig.text12,
               onClose: () => {
-                window.location = "https://www.facebook.com/profile.php?id=100032736788526";
+                window.location = "lnikfacebook";
                 
               },
             })
